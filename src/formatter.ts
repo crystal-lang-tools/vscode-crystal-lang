4dd5f55d2fbe1620edd560612ced9ee516e690d6
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-11 15:45:59 -0500

    Refactoring formatter and linter

--- src/formatter.ts
+++ src/formatter.ts
@@ -3,57 +3,34 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as child_process from 'child_process';
 
-import { validateFile } from './linter'
+import { analyzeDocument } from './linter'
 
 function execFormatter(document: vscode.TextDocument) {
     return new Promise(function (resolve, reject) {
-        child_process.exec(`crystal tool format ${document.fileName}~`, (err, response) => {
-            validateFile(document);
-            resolve(response);
+        let child = child_process.spawn('crystal', ['tool', 'format', '-f', 'json', '-']);
+        child.stdin.write(document.getText());
+        child.stdout.on('data', (data) => {
+            resolve(data);
         });
-    });
-}
-
-function readResult(document: vscode.TextDocument) {
-    let fileName = document.fileName;
-    let editData = [];
-    return new Promise(function (resolve, reject) {
-        fs.readFile(`${fileName}~`, (err, data) => {
-            // Delete temp file
-            fs.unlinkSync(`${fileName}~`);
-            if (data) {
-                let lastLineId = document.lineCount - 1;
-                let range = new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
-                editData = [vscode.TextEdit.replace(range, data.toString())];
-            }
-            resolve(editData);
-        })
+        child.stdin.end();
     });
 }
 
 async function format(document: vscode.TextDocument) {
     let fileName = document.fileName;
-
-    // Create temp file
-    fs.createReadStream(fileName).pipe(fs.createWriteStream(`${fileName}~`));
-
-    let err = await execFormatter(document);
-
-    if (err) {
-        console.error(err)
-        return;
-    } else {
-        return await readResult(document);
+    let response = await execFormatter(document);
+    let editData: vscode.TextEdit[] = [];
+    if (analyzeDocument(response.toString(), fileName)) {
+        let lastLineId = document.lineCount - 1;
+        let range = new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+        editData = [vscode.TextEdit.replace(range, response.toString())];
     }
+    return editData;
 }
 
 // Formatter implemented using API
 export const formatRegistration = vscode.languages.registerDocumentFormattingEditProvider('crystal', {
     provideDocumentFormattingEdits(document: vscode.TextDocument): Promise<vscode.TextEdit[]> {
-        if (document.isDirty) {
-            document.save();
-        }
-        document.uri
         return format(document).then((data) => {
             return data;
         })
--- src/linter.ts
+++ src/linter.ts
@@ -2,6 +2,48 @@
 import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 
+function validateFile(document: vscode.TextDocument) {
+    let fileName = document.fileName;
+    let config = vscode.workspace.getConfiguration('crystal-lang');
+    let problemsLimit = config['maxNumberOfProblems'];
+    if (!config['verifyFiles']) {
+        return;
+    }
+    if (config['mainFile'] !== "") {
+        fileName = config['mainFile'];
+    }
+    child_process.exec(`crystal build --no-color --no-codegen -f json ${fileName}`, (err, response) => {
+        analyzeDocument(response, '', problemsLimit);
+    })
+}
+
+interface CrystalError {
+    file: string;
+    size: number;
+    line: number;
+    column: number;
+    message: string;
+}
+
+export function analyzeDocument(response: string, fileName, problemsLimit = 20) {
+    diagnosticCollection.clear();
+    let diagnostics = [];
+    if (response.startsWith("[{\"file\":\"")) {
+        let results: CrystalError[] = JSON.parse(response);
+        let length = Math.min(problemsLimit, results.length);
+        for (let problemNumber = 0; problemNumber < length; problemNumber += 1) {
+            let problem = results[problemNumber];
+            let range = new vscode.Range(problem.line - 1, problem.column - 1, problem.line - 1, (problem.column + (problem.size || 0) - 1));
+            let diagnostic = new vscode.Diagnostic(range, problem.message, vscode.DiagnosticSeverity.Error);
+            diagnostics.push([vscode.Uri.file(problem.file || fileName), [diagnostic]]);
+        }
+    } else {
+        return true;
+    }
+    // Send the computed diagnostics to VSCode.
+    diagnosticCollection.set(diagnostics);
+}
+
 export const diagnosticCollection = vscode.languages.createDiagnosticCollection('crystal');
 
 export const openValidate = vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
@@ -15,30 +57,3 @@ export const saveValidate = vscode.workspace.onDidSaveTextDocument((document: vs
         validateFile(document);
     }
 });
-
-export function validateFile(document: vscode.TextDocument) {
-    let config = vscode.workspace.getConfiguration('crystal-lang');
-    if (!config['verifyFiles']) {
-        return;
-    }
-    diagnosticCollection.clear();
-    let diagnostics = [];
-    let fileName = document.fileName;
-    if (config['mainFile'] !== "") {
-        fileName = config['mainFile'];
-    }
-    child_process.exec(`crystal build --no-color --no-codegen -f json ${fileName}`, (err, response) => {
-        if (response && !response.startsWith("Warning:")) {
-            let results = JSON.parse(response);
-            let length = Math.min(config['maxNumberOfProblems'], results.length);
-            for (let problemNumber = 0; problemNumber < length; problemNumber += 1) {
-                let problem = results[problemNumber];
-                let range = new vscode.Range(problem.line - 1, problem.column - 1, problem.line - 1, (problem.column + (problem.size || 0) - 1));
-                let diagnostic = new vscode.Diagnostic(range, problem.message, vscode.DiagnosticSeverity.Error);
-                diagnostics.push([vscode.Uri.file(problem.file), [diagnostic]]);
-            }
-        }
-        // Send the computed diagnostics to VSCode.
-        diagnosticCollection.set(diagnostics);
-    })
-}
