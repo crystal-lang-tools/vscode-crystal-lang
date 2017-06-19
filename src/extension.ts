fbf18cbb6371a2996d8486523cc626282acca05e
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-18 23:15:38 -0500

    Fix win32 errors

--- src/extension.ts
+++ src/extension.ts
@@ -3,19 +3,22 @@ import { ExtensionContext } from 'vscode';
 import { platform } from 'os';
 
 import { languageConfiguration } from './configuration'
-import { documentFormatting } from './formatting'
-import { diagnosticCollection, diagnosticOnOpen, diagnosticOnSave } from './diagnostic'
+// import { documentFormatting } from './formatting'
+// import { diagnosticCollection, diagnosticOnOpen, diagnosticOnSave } from './diagnostic'
 
 export function activate(context: ExtensionContext) {
 	context.subscriptions.push(languageConfiguration);
 
 	// Crystal doesn't support Windows yet
-	if (platform() === 'win32') { return; }
+	if (platform() !== 'win32') {
+		const formatting = require('./formatting');
+		const diagnostic = require('./diagnostic');
+		context.subscriptions.push(formatting.documentFormatting);
+		context.subscriptions.push(diagnostic.diagnosticCollection);
+		context.subscriptions.push(diagnostic.diagnosticOnOpen);
+		context.subscriptions.push(diagnostic.diagnosticOnSave);
+	}
 
-	context.subscriptions.push(documentFormatting);
-	context.subscriptions.push(diagnosticCollection);
-	context.subscriptions.push(diagnosticOnOpen);
-	context.subscriptions.push(diagnosticOnSave);
 }
 
 export function deactivate() {
