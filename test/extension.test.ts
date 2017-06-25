f7ab430d7d8cd65dbec86352ad33ecdee9cf85f3
Faustino Aguilar <faustinoaq@gmail.com>
2017-06-25 03:39:03 -0500

    Add unit tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,19 +4,31 @@
 //
 
 // The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
+import * as assert from 'assert'
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
-import * as myExtension from '../src/crystalMain';
+import { CrystalDiagnosticTest } from './crystalDiagnosticTest'
 
 // Defines a Mocha test suite to group tests of similar kind together
+// ---------------------------------------------------------------------------------
+// Run mocha with ./node_modules/mocha/bin/mocha --ui tdd out/test/extension.test.js
+// ---------------------------------------------------------------------------------
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
-});
+	const CODE_WITH_ERROR = 'variable = {}'
+	const CODE_BAD_FORMAT = 'variable  :   String  =  ""'
+	const CODE_FORMATED = 'variable : String = ""\n'
+	const EMPTY_HASH_ERROR = `[{"file":"","line":1,"column":12,"size":null,"message":"for empty hashes use '{} of KeyType => ValueType'"}]\n`
+	let diagnostic = new CrystalDiagnosticTest()
+
+	test("crystal error", async () => {
+		let response = await diagnostic.crystalDoDiagnostic(CODE_WITH_ERROR)
+		assert.equal(response.toString(), EMPTY_HASH_ERROR)
+	})
+
+	test("crystal formatting", async () => {
+		let response = await diagnostic.crystalDoDiagnostic(CODE_BAD_FORMAT)
+		assert.equal(response.toString(), CODE_FORMATED)
+	})
+})
