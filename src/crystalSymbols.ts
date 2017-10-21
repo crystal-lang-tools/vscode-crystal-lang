59bbce994f30e4eede88f3c1c99562fc867e5010
Faustino Aguilar <faustinoaq@gmail.com>
2017-10-21 03:28:31 -0500

    Refactor CrystalDocumentSymbolProvider methods
    * Change local functions by methods
    * Change local variables by attributes

--- src/crystalSymbols.ts
+++ src/crystalSymbols.ts
@@ -5,49 +5,62 @@ import * as vscode from "vscode"
  */
 export class CrystalDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
-	// Return symbols result to VSCode
-	async provideDocumentSymbols(document: vscode.TextDocument) {
+	private countEnds: number
+	private containers: any[]
+	private symbols: vscode.SymbolInformation[]
+	private document: vscode.TextDocument
 
-		let containers = []
-		let countEnds = 0
-		let symbols: vscode.SymbolInformation[] = []
-		let matrixText = document.getText().split("\n")
+	/**
+	 * Create new symbol and append to symbols list
+	 */
+	newSymbol(name, kind, container, line, matchData) {
+		// -------------------
+		// TODO: count columns
+		// -------------------
+		let location = new vscode.Location(this.document.uri, new vscode.Position(line, 0))
+		let symbolInfo = new vscode.SymbolInformation(name, kind, container, location)
+		this.symbols.push(symbolInfo)
+	}
 
-		// Create new symbol and append to symbols list
-		function newSymbol(name, kind, container, line, matchData) {
-			// -------------------
-			// TODO: count columns
-			// -------------------
-			let location = new vscode.Location(document.uri, new vscode.Position(line, 0))
-			let symbolInfo = new vscode.SymbolInformation(name, kind, container, location)
-			symbols.push(symbolInfo)
+	/**
+	 * Set container for current symbol
+	 */
+	setContainer(matchData) {
+		if (this.containers[this.countEnds]) {
+			this.containers.pop()
 		}
+		this.containers.push(matchData)
+		this.countEnds += 1
+	}
 
-		// Set container for current symbol
-		function setContainer(matchData) {
-			if (containers[countEnds]) {
-				containers.pop()
-			}
-			containers.push(matchData)
-			countEnds += 1
-		}
-
-		// Increment counter when an assignement expresion is found
-		function incrementEndsCountIfKeywordIn(element) {
-			const keywords = ["begin", "if", "case", "unless"]
-			for (let keyword of keywords) {
-				let keyFound = (() => {
-					if (keyword == "begin") {
-						return new RegExp(keyword + "$").test(element)
-					}
-					return new RegExp("= " + keyword + " .*$").test(element)
-				})()
-				if (keyFound) {
-					countEnds += 1
-					break
+	/**
+	 * Increment counter when an assignement expresion is found
+	 */
+	incrementEndsCountIfKeywordIn(element) {
+		const keywords = ["begin", "if", "case", "unless"]
+		for (let keyword of keywords) {
+			let keyFound = (() => {
+				if (keyword == "begin") {
+					return new RegExp(keyword + "$").test(element)
 				}
+				return new RegExp("= " + keyword + " .*$").test(element)
+			})()
+			if (keyFound) {
+				this.countEnds += 1
+				break
 			}
 		}
+	}
+
+	/**
+	 * Return symbols result to VSCode
+	 */
+	async provideDocumentSymbols(document: vscode.TextDocument) {
+		let matrixText = document.getText().split("\n")
+		this.document = document
+		this.containers = []
+		this.countEnds = 0
+		this.symbols = []
 
 		// Search symbol line by line, ignoring comments and empty lines
 		for (let [index, element] of matrixText.entries()) {
@@ -55,60 +68,59 @@ export class CrystalDocumentSymbolProvider implements vscode.DocumentSymbolProvi
 			let comment = element.match(/^\s*#.*$/)
 			if (comment == null && element != "") {
 				if (matchData = element.match(/^\s*(abstract\s+)?(private\s+|protected\s+)?(def|fun) ([^\s\(\)\:]+).*$/)) {
-					newSymbol(matchData[4], vscode.SymbolKind.Function, containers[countEnds - 1], index, matchData)
+					this.newSymbol(matchData[4], vscode.SymbolKind.Function, this.containers[this.countEnds - 1], index, matchData)
 					if (matchData[1] === undefined) {
-						countEnds += 1
+						this.countEnds += 1
 					}
 				} else if (matchData = element.match(/^\s*(private\s+)?(macro) ([^\s\(\)\:]+).*$/)) {
-					newSymbol(matchData[3], vscode.SymbolKind.Function, containers[countEnds - 1], index, matchData.index)
-					countEnds += 1
+					this.newSymbol(matchData[3], vscode.SymbolKind.Function, this.containers[this.countEnds - 1], index, matchData.index)
+					this.countEnds += 1
 				} else if (matchData = element.match(/^\s*(abstract\s+)?(private\s+)?(class) ([A-Z][^\s\(\)]*).*$/)) {
-					newSymbol(matchData[4], vscode.SymbolKind.Class, containers[countEnds - 1], index, matchData.index)
-					setContainer(matchData[4])
+					this.newSymbol(matchData[4], vscode.SymbolKind.Class, this.containers[this.countEnds - 1], index, matchData.index)
+					this.setContainer(matchData[4])
 				} else if (matchData = element.match(/^\s*(private\s+|protected\s+)?(class_)?(property|getter|setter)(!|\?)? ([^\s\(\)\:]+).*$/)) {
-					newSymbol(matchData[5], vscode.SymbolKind.Property, containers[countEnds - 1], index, matchData.index)
+					this.newSymbol(matchData[5], vscode.SymbolKind.Property, this.containers[this.countEnds - 1], index, matchData.index)
 				} else if (matchData = element.match(/^\s*(abstract\s+)?(private\s+)?(struct|record) ([A-Z][^\s\(\)]*).*$/)) {
-					newSymbol(matchData[4], vscode.SymbolKind.Struct, containers[countEnds - 1], index, matchData.index)
+					this.newSymbol(matchData[4], vscode.SymbolKind.Struct, this.containers[this.countEnds - 1], index, matchData.index)
 					if (matchData[1] === undefined && matchData[3] !== "record") {
-						setContainer(matchData[4])
+						this.setContainer(matchData[4])
 					}
 				} else if (matchData = element.match(/^\s*(private\s+)?(module) ([A-Z][^\s\(\)]*).*$/)) {
-					newSymbol(matchData[3], vscode.SymbolKind.Module, containers[countEnds - 1], index, matchData.index)
-					setContainer(matchData[3])
+					this.newSymbol(matchData[3], vscode.SymbolKind.Module, this.containers[this.countEnds - 1], index, matchData.index)
+					this.setContainer(matchData[3])
 				} else if (matchData = element.match(/^\s*(private\s+)?(lib) ([A-Z][^\s\(\)\:]*).*$/)) {
-					newSymbol(matchData[3], vscode.SymbolKind.Module, containers[countEnds - 1], index, matchData.index)
-					countEnds += 1
+					this.newSymbol(matchData[3], vscode.SymbolKind.Module, this.containers[this.countEnds - 1], index, matchData.index)
+					this.countEnds += 1
 				} else if (matchData = element.match(/^\s*(private\s+)?(enum|union) ([A-Z][^\s\(\)\:]*).*$/)) {
-					newSymbol(matchData[3], vscode.SymbolKind.Enum, containers[countEnds - 1], index, matchData.index)
-					countEnds += 1
+					this.newSymbol(matchData[3], vscode.SymbolKind.Enum, this.containers[this.countEnds - 1], index, matchData.index)
+					this.countEnds += 1
 				} else if (matchData = element.match(/^\s*(alias\s+|type\s+)?([A-Z][^\s\(\)\:]*)\s*=.*$/)) {
-					newSymbol(matchData[2], vscode.SymbolKind.Constant, containers[countEnds - 1], index, matchData.index)
-					incrementEndsCountIfKeywordIn(element)
+					this.newSymbol(matchData[2], vscode.SymbolKind.Constant, this.containers[this.countEnds - 1], index, matchData.index)
+					this.incrementEndsCountIfKeywordIn(element)
 				} else if (matchData = element.match(/^\s*(\w[^\@\s\(\)\:]*)\s+:\s+.*$/)) {
-					newSymbol(matchData[1], vscode.SymbolKind.Variable, containers[countEnds - 1], index, matchData.index)
+					this.newSymbol(matchData[1], vscode.SymbolKind.Variable, this.containers[this.countEnds - 1], index, matchData.index)
 				} else if (matchData = element.match(/^\s*(@\w[^\s\(\)\:]*)\s+:\s+.*$/)) {
-					newSymbol(matchData[1], vscode.SymbolKind.Variable, containers[countEnds - 1], index, matchData.index)
+					this.newSymbol(matchData[1], vscode.SymbolKind.Variable, this.containers[this.countEnds - 1], index, matchData.index)
 				} else if (element.match(/^\s*end(\..*)?$/)) {
-					countEnds -= 1
-					if (countEnds <= 0) {
-						containers = []
+					this.countEnds -= 1
+					if (this.countEnds <= 0) {
+						this.containers = []
 					}
 				} else if (element.match(/^\s*(if|unless|until|while)\s.*$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				} else if (element.match(/^.*=\s+(if|unless)\s.*$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				} else if (element.match(/^\s*(select|case|begin)$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				} else if (element.match(/^.*\sdo(\s\|.*)?$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				} else if (element.match(/^\s*case\s.*$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				} else if (element.match(/^.*\sbegin$/)) {
-					countEnds += 1
+					this.countEnds += 1
 				}
 			}
 		}
-
-		return symbols
+		return this.symbols
 	}
 }
