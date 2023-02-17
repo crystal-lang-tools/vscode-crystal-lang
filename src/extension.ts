33c0fd12302960d09f18f23396e59b84ec18cb21
devnote-dev <devnote.dev75@gmail.com>
2023-02-17 23:03:24 +0000

    fix: prevent unnecessary auto-indent of in

--- src/extension.ts
+++ src/extension.ts
@@ -15,9 +15,9 @@ const selector = <DocumentSelector>[{ language: 'crystal', scheme: 'file' }];
 const configuration = <LanguageConfiguration>{
 	indentationRules: {
 		increaseIndentPattern:
-			/^\s*((begin|(private\s+abstract|private|abstract)\s+(class|struct)|class|struct|(private|protected)\s+def|def|fun|macro|else|elsif|ensure|for|if|module|rescue|unless|until|when|in|while|case)|([^#]*\sdo\b)|([^#]*=\s*(case|if|unless)))\b([^#\{;]|("|'|\/).*\4)*(#.*)?$/,
+			/^\s*((begin|(private\s+abstract|private|abstract)\s+(module|class|struct|enum)|class|struct|(private|protected)\s+def|def|fun|macro|else|elsif|ensure|for|if|module|enum|rescue|unless|until|when|while|case)|([^#]*\sdo\b)|([^#]*=\s*(case|if|unless)))\b([^#\{;]|("|'|\/).*\4)*(#.*)?$/,
 		decreaseIndentPattern:
-			/^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when|in)\b)/,
+			/^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when|(?:case[\s\S\n]+)in)\b)/,
 	},
 	wordPattern:
 		/(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/,
