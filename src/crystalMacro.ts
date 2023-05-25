5ae9e9dc84a2edd2ccf60f84763f5b40a1104e51
Margret Riegert <margret@eowyn.net>
2023-05-24 21:48:50 -0400

    Macro expansion on right-click to output pane

--- src/crystalMacro.ts
+++ src/crystalMacro.ts
@@ -2,18 +2,39 @@ import * as vscode from "vscode";
 import { spawnSync } from 'node:child_process';
 
 export async function registerCrystalMacroHoverProvider(context: vscode.ExtensionContext) {
+    let macroExpansionChannel = vscode.window.createOutputChannel("Crystal Macro Expansion", "crystal")
+
     vscode.languages.registerHoverProvider({ scheme: 'file', language: 'crystal' }, {
         provideHover(document, position, token) {
             return expandMacroToHover(document, position);
         }
     })
+
+    vscode.commands.registerCommand('crystal-lang.showMacroExpansion', function() {
+        const activeEditor = vscode.window.activeTextEditor;
+        const document = activeEditor.document;
+        const position = activeEditor.selection.active;
+        const positionString = `${document.uri.path}:${position.line + 1}:${position.character + 1}`;
+
+        let stdout = execExpandMacro(document, position)
+
+        if (stdout === undefined || stdout === "") {
+            return;
+        }
+
+        if (stdout.startsWith("no expansion found")) {
+            stdout = `# No macro expansion for ${positionString}`;
+        } else {
+            stdout = `# Macro expansion for ${positionString}:\n\n${stdout}\n`
+        }
+
+        macroExpansionChannel.appendLine(stdout)
+        macroExpansionChannel.show()
+    })
 }
 
-function expandMacroToHover(document: vscode.TextDocument, position: vscode.Position): vscode.Hover {
+function execExpandMacro(document: vscode.TextDocument, position: vscode.Position): string {
     const config = vscode.workspace.getConfiguration("crystal-lang")
-    if (!config["macroExpansionHover"]) {
-        return;
-    }
 
     let currentWorkspacePath = vscode.workspace.getWorkspaceFolder(document.uri).uri.path
 
@@ -45,8 +66,19 @@ function expandMacroToHover(document: vscode.TextDocument, position: vscode.Posi
     }
 
     let stdout = result.output.join('\n').replace(/^\n+|\n+$/g, '');
+
+    return stdout
+}
+
+function expandMacroToHover(document: vscode.TextDocument, position: vscode.Position): vscode.Hover {
+    const config = vscode.workspace.getConfiguration("crystal-lang")
+    if (!config["macroExpansionHover"]) {
+        return;
+    }
+
+    let stdout = execExpandMacro(document, position)
+
     if (stdout.startsWith("no expansion found")) {
-        // console.log(`No macro expansion at ${filePath}:${line}:${column}`)
         return;
     }
 
