333d236f332d8b8af0a5b3ecc3872c1761b3e961
Margret Riegert <margret@eowyn.net>
2024-02-02 21:25:04 -0500

    Formatting, run problems tool on opening a file

--- src/macro.ts
+++ src/macro.ts
@@ -4,49 +4,49 @@ import { crystalOutputChannel, execAsync, findProblems, getCompilerPath, getCurs
 export const macroOutputChannel = window.createOutputChannel("Crystal Macro", "markdown")
 
 export function registerMacroExpansion() {
-    commands.registerCommand('crystal-lang.showMacroExpansion', async function () {
-        const activeEditor = window.activeTextEditor;
-        const document = activeEditor.document;
-        const position = activeEditor.selection.active;
+  commands.registerCommand('crystal-lang.showMacroExpansion', async function () {
+    const activeEditor = window.activeTextEditor;
+    const document = activeEditor.document;
+    const position = activeEditor.selection.active;
 
-        const response = await spawnMacroExpandTool(document, position)
-            .then((resp) => {
-                if (resp) {
-                    return resp
-                } else {
-                    return undefined
-                }
-            })
-
-        if (response) {
-            macroOutputChannel.appendLine("```crystal\n" + response + "```\n")
+    const response = await spawnMacroExpandTool(document, position)
+      .then((resp) => {
+        if (resp) {
+          return resp
         } else {
-            macroOutputChannel.appendLine("# No macro expansion found")
+          return undefined
         }
-        macroOutputChannel.show()
-    })
+      })
+
+    if (response) {
+      macroOutputChannel.appendLine("```crystal\n" + response + "```\n")
+    } else {
+      macroOutputChannel.appendLine("# No macro expansion found")
+    }
+    macroOutputChannel.show()
+  })
 }
 
 export async function spawnMacroExpandTool(document: TextDocument, position: Position): Promise<string | void> {
-    const compiler = await getCompilerPath();
-    const main = await getShardMainPath(document);
-    const cursor = getCursorPath(document, position);
-    const folder = getWorkspaceFolder(document.uri);
-    const config = workspace.getConfiguration('crystal-lang');
+  const compiler = await getCompilerPath();
+  const main = await getShardMainPath(document);
+  const cursor = getCursorPath(document, position);
+  const folder = getWorkspaceFolder(document.uri);
+  const config = workspace.getConfiguration('crystal-lang');
 
-    const cmd = `${shellEscape(compiler)} tool expand ${shellEscape(main)} --cursor ${shellEscape(cursor)} ${config.get<string>("flags")}`
+  const cmd = `${shellEscape(compiler)} tool expand ${shellEscape(main)} --cursor ${shellEscape(cursor)} ${config.get<string>("flags")}`
 
-    crystalOutputChannel.appendLine(`[Macro Expansion] (${folder.name}) $ ` + cmd)
-    return await execAsync(cmd, folder.uri.fsPath)
-        .then((response) => {
-            return response;
+  crystalOutputChannel.appendLine(`[Macro Expansion] (${folder.name}) $ ` + cmd)
+  return await execAsync(cmd, folder.uri.fsPath)
+    .then((response) => {
+      return response;
+    })
+    .catch(async (err) => {
+      const new_cmd = cmd + ' -f json'
+      await execAsync(new_cmd, folder.uri.fsPath)
+        .catch((err) => {
+          findProblems(err.stderr, document.uri)
+          crystalOutputChannel.appendLine(`[Macro Expansion] Error: ${err.message}`)
         })
-        .catch(async (err) => {
-            const new_cmd = cmd + ' -f json'
-            await execAsync(new_cmd, folder.uri.fsPath)
-                .catch((err) => {
-                    findProblems(err.stderr, document.uri)
-                    crystalOutputChannel.appendLine(`[Macro Expansion] Error: ${err.message}`)
-                })
-        });
+    });
 }
